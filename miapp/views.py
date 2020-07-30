23c5d3e6bc2498b4dc4d73a4ab7d2f5135607b9a
Gomez Elias <elias.f.g.61@gmail.com>
2020-07-30 17:44:54 -0500

    practica LP3

---
+++ miapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+
+layout = """
+"""
+
+def index(request):
+    return render(request,'index.html')
+
+def saludo(request):
+    return render(request,'saludo.html')
+
+def rango(request):
+    a = 10
+    b = 20
+    resultado = f"""
+        <h2> Numeros de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
+
+def rango2(request,a=0,b=100):
+    if a>b:
+        return redirect('rango2',a=b, b=a)
+
+    resultado = f"""
+        <h2> Numeros de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
